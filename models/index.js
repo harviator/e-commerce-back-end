11a252801f555835538886044cd2855687039256
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-26 12:56:14 -0400

    Made changes so the categeory delete route worked again

--- models/index.js
+++ models/index.js
@@ -5,22 +5,24 @@ const ProductTag = require('./ProductTag');
 
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+
 });
 
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+ 
 });
 
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
-  onDelete: 'CASCADE',
+  
 })
 
 module.exports = {
